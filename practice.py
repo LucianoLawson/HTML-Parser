72fc4de65f1de29205aa613c26df8fe9b0652697
luciano lawson <lucianoglawson@yahoo.com>
2024-03-29 10:14:37 -0500

    added file for practicing

---
+++ practice.py
@@ -0,0 +1,17 @@
+""" number = 99
+for i in range(number, -1, -1):  # Start at 99, go to 0 (inclusive), step by -1
+    print(i , "bottles of beer on the wall")
+    print(i, "bottles of beer")
+    print("Take one down, pass it around")
+    print(i, "bottles of beer on the wall") """
+
+
+
+from turtle import *
+
+width(6)
+right(90)
+forward(100)
+left(90)
+forward(100)
+bye()
